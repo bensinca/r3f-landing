4806d6f5f335a5b7b0901ae97ccc554535c28715
bensinca <beni.sinca@gmail.com>
2024-05-09 19:42:26 +0100

    Set scene

---
+++ src/Overlay.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { a } from '@react-spring/web'
+
+export default function Overlay({ fill }) {
+  // TODO: SET A FIGMA EXPORT UNTIL ANIMATION IS READY
+  return (
+    <div className="overlay">
+      <a.svg viewBox="0 0 583 720" fill={fill} xmlns="http://www.w3.org/2000/svg">
+            
+      </a.svg>
+    </div>
+  )
+}
